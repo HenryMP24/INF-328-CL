55525c47c31aba0ed4d6b0f8b056bb591cc5cc34
HenryMP24 <68402585+HenryMP24@users.noreply.github.com>
2020-10-18 21:14:56 -0400

    calculadora en java

---
+++ exa2/src/exa2/Exa2.java
@@ -0,0 +1,16 @@
+
+package exa2;
+
+import java.util.Scanner;
+public class Exa2 {
+
+  
+    public static void main(String[] args) {
+        //evaluador eval=new evaluador();
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("Digite la Expresion a evaluar:");
+        //String infija=sc.next();
+        //System.out.println("El resultado es:"+eval.evaluar(infija));
+    }
+    
+}
